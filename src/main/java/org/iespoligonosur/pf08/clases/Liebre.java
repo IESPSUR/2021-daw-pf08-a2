3a4076f114139b5e59a9bea6945003fc45d9716a
RubenCF <ruben.castellano.fernandez.al@iespoligonosur.org>
2020-12-08 15:42:27 +0100

    añadido metodo de movimiento para tortuga y liebre

--- src/main/java/org/iespoligonosur/pf08/clases/Liebre.java
+++ src/main/java/org/iespoligonosur/pf08/clases/Liebre.java
@@ -1,5 +1,52 @@
 package org.iespoligonosur.pf08.clases;
 
+import java.util.Scanner;
+
 public class Liebre {
 
+	int dado6;
+	Scanner teclado;
+	
+	/**
+	 * Constructor para pasar por teclado
+	 * @param teclado
+	 */
+	public Liebre(Scanner teclado) {
+		this.teclado=teclado;
+	}
+	/**
+	 * metodo para el lanzamiento de dado de 6 caras y cribado de opciones
+	 * @param dado6
+	 * @param teclado
+	 * @return
+	 */
+	public int MovimientoLiebre(int dado6, Scanner teclado) {
+		dado6 = (int) Math.floor(Math.random() * 6+1);
+		if (dado6==3)
+			return dado6;
+		else {
+			System.out.println("Adivina lo que a salido");
+			int adivina= teclado.nextInt();
+			if (adivina==dado6) {
+				return dado6;
+			}else {
+				System.out.println("Lo sentimos salio " + dado6+ " intentelo el proximo turno");
+				dado6=0;
+				return dado6;
+			}
+			
+		}
+	}
+	
+	/**
+	 * para probar los lanzamientos si funciona (añadir static a los metodos y variables, y comentar el escaner)
+	 */
+	/*public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		dado6=MovimientoLiebre(dado6, teclado);
+		System.out.println(dado6);
+		
+		teclado.close
+	}*/
 }

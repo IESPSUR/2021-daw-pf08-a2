74de4d9ee5493332be04d5d57deba3646ea2f299
RubenCF <ruben.castellano.fernandez.al@iespoligonosur.org>
2020-12-09 17:50:24 +0100

    metodo para lanzar dado en el correcaminos

--- src/main/java/org/iespoligonosur/pf08/clases/CorreCaminos.java
+++ src/main/java/org/iespoligonosur/pf08/clases/CorreCaminos.java
@@ -1,5 +1,37 @@
 package org.iespoligonosur.pf08.clases;
 
+
 public class CorreCaminos {
+	static int dado10;
+	
+
+	/**
+	 * metodo para el lanzamiento de dado de 10 caras y cribado de opciones
+	 * @param dado6
+	 * @param teclado
+	 * @return
+	 */
+	public int DadoLiebre(int dado10) {
+		int resto;
+		
+		dado10 = (int) Math.floor(Math.random() * 10+1);
+		resto = dado10 % 2;
+		if (resto==0)
+			return dado10;
+		else {
+			dado10=0;
+			return dado10;
+		}
+	}
+	
+	/**
+	 * para probar los lanzamientos si funciona (añadir static a los metodos y variables)
+	 */
+	/*public static void main(String[] args) {
+
+		dado10=DadoLiebre(dado10);
+		System.out.println(dado10);
+		
+	}*/
 
 }

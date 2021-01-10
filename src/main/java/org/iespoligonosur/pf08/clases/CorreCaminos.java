e75fda59043a6788a5dce42fd4f82cf82815c93e
sokayna <bagari.sokayna.al@ies poligonosur.org>
2021-01-10 14:14:43 +0100

    crear constructor para la clase CorreCamino

--- src/main/java/org/iespoligonosur/pf08/clases/CorreCaminos.java
+++ src/main/java/org/iespoligonosur/pf08/clases/CorreCaminos.java
@@ -2,4 +2,41 @@ package org.iespoligonosur.pf08.clases;
 
 public class CorreCaminos {
 
+	
+	
+	private int dado;
+	
+	public CorreCaminos() {
+		
+	}
+
+	public CorreCaminos(int dado) {
+		super();
+		this.dado = dado;
+	}
+
+	/**
+	 * @return the dado
+	 */
+	public int getDado() {
+		return dado;
+	}
+
+	/**
+	 * @param dado the dado to set
+	 */
+	public void setDado(int dado) {
+		this.dado = dado;
+	}
+	public  void dadoCorreCamino() {
+		int dado = (int) Math.floor(Math.random()*100);
+		if ( dado%2==0) {
+			
+		}
+		
+		
+	}
+	
+		
+	
 }
